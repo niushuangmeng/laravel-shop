b743fea33839c030eb9ec34853ca98fa59a62b54
niushuangmeng <niushuangmeng@gmail.com>
2019-08-09 03:33:43 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,8 @@ Route::get('/', function () {
 Route::get('/','PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
+
+Route::group(['middleware' => ['auth','verified']],
+function(){
+  Route::get('user_addresses','UserAddressesController@index')->name('user_addresses.index');
+});
