8654568ae26c9888e4660906694f18292ac1d073
niushuangmeng <niushuangmeng@gmail.com>
2019-12-03 06:46:55 +0000

    引入支付库

--- app/Exceptions/InternalException.php
+++ app/Exceptions/InternalException.php
@@ -9,7 +9,7 @@ class InternalException extends Exception
 {
     protected $msgForUser;
 
-    public function __construct(sting $message,sting $msgForUser='系统内部错误',int $code = 500)
+    public function __construct(sting $message,sting $msgForUser='系统内部错误',int $code = 500)//第一个参数就是原本应该有的异常信息,第二个参数是展示给用户的信息
     {
       parent::__construct($message,$code);
       $this->$msgForUser=$msgForUser;
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -67,6 +67,7 @@
       $('.product-info .price span').text($(this).data('price'));
       $('.product-info .stock').text('库存：' + $(this).data('stock') + '件');
     });
+
     $('.btn-favor').click(function () {
      // 发起一个 post ajax 请求，请求 url 通过后端的 route() 函数生成。
      axios.post('{{ route('products.favor', ['product' => $product->id]) }}')
@@ -86,43 +87,43 @@
        });
    });
 
-   // 加入购物车按钮点击事件
-    $('.btn-add-to-cart').click(function () {
+   {{--// 加入购物车按钮点击事件--}}
+    {{--$('.btn-add-to-cart').click(function () {--}}
 
-      // 请求加入购物车接口
-      axios.post('{{ route('cart.add') }}', {
-        sku_id: $('label.active input[name=skus]').val(),
-        amount: $('.cart_amount input').val(),
-      })
-        .then(function () { // 请求成功执行此回调
-          swal('加入购物车成功', '', 'success')
-          .then(function() {
-          location.href = '{{ route('cart.index') }}';
-        });
-        }, function (error) { // 请求失败执行此回调
-          if (error.response.status === 401) {
+      {{--// 请求加入购物车接口--}}
+      {{--axios.post('{{ route('cart.add') }}', {--}}
+        {{--sku_id: $('label.active input[name=skus]').val(),--}}
+        {{--amount: $('.cart_amount input').val(),--}}
+      {{--})--}}
+        {{--.then(function () { // 请求成功执行此回调--}}
+          {{--swal('加入购物车成功', '', 'success')--}}
+          {{--.then(function() {--}}
+          {{--location.href = '{{ route('cart.index') }}';--}}
+        {{--});--}}
+        {{--}, function (error) { // 请求失败执行此回调--}}
+          {{--if (error.response.status === 401) {--}}
 
-            // http 状态码为 401 代表用户未登陆
-            swal('请先登录', '', 'error');
+            {{--// http 状态码为 401 代表用户未登陆--}}
+            {{--swal('请先登录', '', 'error');--}}
 
-          } else if (error.response.status === 422) {
+          {{--} else if (error.response.status === 422) {--}}
 
-            // http 状态码为 422 代表用户输入校验失败
-            var html = '<div>';
-            _.each(error.response.data.errors, function (errors) {
-              _.each(errors, function (error) {
-                html += error+'<br>';
-              })
-            });
-            html += '</div>';
-            swal({content: $(html)[0], icon: 'error'})
-          } else {
+            {{--// http 状态码为 422 代表用户输入校验失败--}}
+            {{--var html = '<div>';--}}
+            {{--_.each(error.response.data.errors, function (errors) {--}}
+              {{--_.each(errors, function (error) {--}}
+                {{--html += error+'<br>';--}}
+              {{--})--}}
+            {{--});--}}
+            {{--html += '</div>';--}}
+            {{--swal({content: $(html)[0], icon: 'error'})--}}
+          {{--} else {--}}
 
-            // 其他情况应该是系统挂了
-            swal('系统错误', '', 'error');
-          }
-        })
-    });
+            {{--// 其他情况应该是系统挂了--}}
+            {{--swal('系统错误', '', 'error');--}}
+          {{--}--}}
+        {{--})--}}
+    {{--});--}}
 
 
   });
